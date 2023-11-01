c826642cf689b87553219ad099b17997acf875d2
ZIROH KATANA MAE <tellazone0@gmail.com>
2023-11-01 21:44:09 +0300

    Add files via upload

---
+++ inputs inpython.py
@@ -0,0 +1,36 @@
+print('Good morning!' )
+print('it is rainy today')
+
+print('Good morning!', end= ' ')
+print('it is rainy today')
+
+print('New year', 2023, 'see you soon!' , sep=' ,')
+
+number = -10.6
+name = "power LP"
+#print literals
+print(5)
+#print variables
+print(number)
+print(name)
+
+#Example:print concatenated strings
+#we can also join two string together inside the print() statement. for example
+
+print('powerLP is ' + 'awesome.')
+
+#here
+#the + operator joins two strings 'PLP is' and 'awesoma'
+#the print() fuction print te joined string
+#Output formating
+
+x = 5
+y = 10
+print('The value of x is {} and the y is {}'.format(x,y))
+
+#Syntax of input()
+#input(prompt)
+
+num = input('5')
+print('5:', num)
+print('Data typeof num:', type(num))
